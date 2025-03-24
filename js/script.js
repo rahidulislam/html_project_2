e1a64e501896e94a6910d92a4ebea1c5445f2142
Rahidul Islam <rahiseli@outlook.com>
2025-03-24 17:23:10 +0600

    carousel is working but needs some modification

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  const nextBtn = document.querySelectorAll(".nav-btn");
+  const carouselSlides = document.querySelector(".carousel-slides");
 
+  let currentIndex = 0;
+  const slideCount = slides.length;
+
+  function updateCarousel() {
+    const offset = -currentIndex * 60;
+    carouselSlides.style.transform = `translateX(${offset}%)`;
+    slides.forEach((slide, index) => {
+      if (index === currentIndex) {
+        slide.classList.add("active");
+      } else {
+        slide.classList.remove("active");
+      }
+    });
+    nextBtn.forEach((btn, index) => {
+      if (index === currentIndex) {
+        btn.classList.add("active");
+      } else {
+        btn.classList.remove("active");
+      }
+    });
+  }
+
+  nextBtn.forEach((btn, index) => {
+    btn.addEventListener("click", function () {
+      currentIndex = index;
+      updateCarousel();
+    });
+  });
+
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % slideCount;
+    updateCarousel();
+  }, 5000);
+  updateCarousel();
+});
